5dbb044c15929a705a77a72e46d33145b5139ce8
thisisgurkaran <gurkaran.12@gmail.com>
2023-03-06 22:21:02 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(3000, () => {
+  console.log("server running at 3000");
+});
